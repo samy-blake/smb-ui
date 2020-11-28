83adf1d8b352e49b34a8bd5a13f52c8d88d6100c
Sören Balke <balkesoeren@googlemail.com>
2020-11-28 19:06:09 +0100

    update package.json

--- projects/smb-ui/src/public-api.ts
+++ projects/smb-ui/src/public-api.ts
@@ -3,4 +3,7 @@
  */
 
 export * from './lib/smb-ui.service';
+export * from './lib/ngx-smb-ui-content/ngx-smb-ui-content.component';
+export * from './lib/ngx-smb-ui-image-button/ngx-smb-ui-image-button.component';
+export * from './lib/ngx-smb-ui-link/ngx-smb-ui-link.component';
 export * from './lib/smb-ui.module';
